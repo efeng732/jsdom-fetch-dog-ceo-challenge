e600e39f15b66beb8cb8fb67227398bb61396b10
Erwin <efeng732@gmail.com>
2021-01-18 20:58:25 -0500

    commit tree 67ab8cf87e6c3eb88fcb4fb30360f8c0ec8b4fc9

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+function loadImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+    fetch(imgURL)
+    .then(res => res.json)
+    .then((imgObj) => {
+        imgObj.message.forEach(img => addImage(img)) 
+    } )
+
+}
+
+function addImage(dogURL){
+    let container = document.querySelector('#dog-image-container');
+    let newImg = document.createElement('img');
+    newImg.src = dogURL; 
+
+    container.append(newImg);
+
+
+}   
